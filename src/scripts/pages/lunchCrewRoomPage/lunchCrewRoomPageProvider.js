1c3b45bd0d8328b4de1f56947c9ac81d4f00f34f
Jonathan Darling <jmdarling@me.com>
2016-10-01 21:32:10 -0500

    Basic logic for showing and choosing a winner.

--- src/scripts/pages/lunchCrewRoomPage/lunchCrewRoomPageProvider.js
+++ src/scripts/pages/lunchCrewRoomPage/lunchCrewRoomPageProvider.js
@@ -21,11 +21,14 @@ export default (configuration, debug, browserHistory, ioClient) => {
 
       this.destinationAddFormChange = this.destinationAddFormChange.bind(this)
       this.destinationAddFormSubmit = this.destinationAddFormSubmit.bind(this)
+      this.selectDestination = this.selectDestination.bind(this)
       this.updateDestinations = this.updateDestinations.bind(this)
+      this.updateWinningOption = this.updateWinningOption.bind(this)
 
       ioClient.emit('join room', props.params.crewName)
 
       ioClient.on('destination options', this.updateDestinations)
+      ioClient.on('winning option', this.updateWinningOption)
     }
 
     destinationAddFormChange (value) {
@@ -58,9 +61,18 @@ export default (configuration, debug, browserHistory, ioClient) => {
               <DestinationList destinations={this.state.destinations}/>
               <DestinationAddForm onChange={this.destinationAddFormChange} onSubmit={this.destinationAddFormSubmit} value={this.state.destinationAddForm}/>
             </div>
+
+            <div className='lunch-crew-room__destination-selection'>
+              <p className='lunch-crew-room__destination-selection__selected-destination'>{this.state.selectedDestination === '' ? 'No Destination Selected' : this.state.selectedDestination}</p>
+              <button className='lunch-crew-room__destination-selection__select-destination' onClick={this.selectDestination}>Play the Lotto!</button>
+            </div>
           </div>
         </div>
       )
+    }
+
+    selectDestination () {
+      ioClient.emit('pull lever')
     }
 
     updateDestinations (newDestinations) {
@@ -68,6 +80,14 @@ export default (configuration, debug, browserHistory, ioClient) => {
 
       this.setState(Object.assign({}, this.state, {
         destinations: this.state.destinations.concat(newDestinations)
+      }))
+    }
+
+    updateWinningOption (winningOption) {
+      debug(`Received new winning option: ${winningOption}`)
+
+      this.setState(Object.assign({}, this.state, {
+        selectedDestination: winningOption
       }))
     }
   }
